5061e804f4fca4187c9f089ab525c08f2e3ef703
truong <anhtrangdep012@gmail.cocm>
2024-11-10 00:50:56 +0700

    done-njs03-server

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,8 +36,8 @@ exports.login = (req, res, next) => {
           res
             .cookie('access_token', token, {
               httpOnly: true,
-              secure: process.env.NODE_ENV === 'production',
-              sameSite: 'None',
+              // secure: process.env.NODE_ENV === 'production',
+              // sameSite: 'None',
             })
             .status(200)
             .json({ details: { ...otherDetails } });
