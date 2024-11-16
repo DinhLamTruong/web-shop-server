1d1030131896c58c4088ded393507672bd0c4de2
truong <anhtrangdep012@gmail.cocm>
2024-11-16 13:12:49 +0700

    done-njs03-server

--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,7 @@ const { verifyToken } = require('../utils/veryfiToken');
 const route = express.Router();
 
 const productControllers = require('../controllers/product');
+const { verifyToken } = require('../utils/veryfiToken');
 
 route.get('/shop', productControllers.getCategory);
 
@@ -10,10 +11,18 @@ route.get('/:idProduct', productControllers.getProduct);
 
 route.get('/', productControllers.getProducts);
 
-route.post('/add-product', productControllers.postAddProduct);
+route.post('/add-product', verifyToken, productControllers.postAddProduct);
 
-route.put('/edit-product/:idProduct', productControllers.updateProduct);
+route.put(
+  '/edit-product/:idProduct',
+  verifyToken,
+  productControllers.updateProduct
+);
 
-route.delete('/delete-product/:idProduct', productControllers.deleteProduct);
+route.delete(
+  '/delete-product/:idProduct',
+  verifyToken,
+  productControllers.deleteProduct
+);
 
 module.exports = route;
